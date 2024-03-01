6619969139a662df2a05c44e512bb9df2a657e95
Rajpreet Singh <rajpreetsingh@192.168.1.11>
2024-03-01 19:29:27 +0530

    Completed OOPS , studied interface , abstract , throws , static methods.

---
+++ Itermediate/src/Main.java
@@ -0,0 +1,92 @@
+import java.util.*;
+
+public class Main {
+  public static void static_method () {
+    System.out.println("Static here !");
+  }
+  public static void main(String args[]) {
+    Chicken c = new Chicken();
+    System.out.println(c.ears());
+    Name n = Name.Anmol;
+
+    ArrayList<String> a = new ArrayList<String>();
+    a.add("Raj");
+    a.add("Anmol");
+    a.add("Nav");
+    for(int i=0;i<3;i++) {
+      System.out.println(a.get(i));
+    }
+  }
+}
+enum Name {
+  Anmol,
+  Raj,
+  Navdeep
+}
+
+interface Animal {
+  //  String animal="";  // since we cant change we dont put it should be there as const
+  public int legs();
+  public int ears();
+  public int eyes();
+}
+
+interface Protect_Animal {
+  //  public abstract int protect_id();
+  public int protect_id();
+}
+
+class Chicken implements Animal,Protect_Animal {
+  //  Chicken() {
+//    this.animal="Chickden"; // inteface does not allow this changing whatever was declared in interface is final
+//  }
+  @Override
+  public int ears() {
+    Main.static_method();
+    return 2;
+  }
+
+
+
+  @Override
+  public int eyes() {
+    return 2;
+  }
+
+  @Override
+  public int legs() {
+    return 2;
+  }
+
+  @Override
+  public int protect_id() {
+    return 52;
+  }
+}
+
+abstract class Animal2 {
+  String animal="";
+  public abstract int legs();
+  public abstract int ears();
+  public abstract int eyes();
+}
+//class Chicken2 extends Animal2,Protect_Animal {  // not possible
+class Chicken2 extends Animal2 {
+  Chicken2() {
+    this.animal="Chickden";
+  }
+  @Override
+  public int ears() {
+    return 2;
+  }
+
+  @Override
+  public int eyes() {
+    return 2;
+  }
+
+  @Override
+  public int legs() {
+    return 2;
+  }
+}
